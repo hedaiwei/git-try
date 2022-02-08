49d67f364eca3e9c1223bbfe4a0821f5675b11f2
hedaiwei <herandaiwei@gmail.com>
2022-02-08 23:51:39 +0800

    add a new method to the sortedSquaredArray

--- Array/sortedSquaredArray.cpp
+++ Array/sortedSquaredArray.cpp
@@ -13,3 +13,27 @@ vector<int> sortedSquaredArray(vector<int> array) {
 	sort( sortedSquares.begin() , sortedSquares.end() );
   return sortedSquares;
 }
+===========solution 2===============
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+vector<int> sortedSquaredArray(vector<int> array) {
+  vector<int> sortedSquares( array.size() , 0 );
+	int smallerValueIdx = 0;
+	int largerValueIdx = array.size() - 1;
+	for( int idx = array.size()-1 ; idx >= 0; idx-- ){
+		int smallerValue = array[ smallerValueIdx ];
+		int largerValue = array[ largerValueIdx ];
+		if( abs( smallerValue ) > abs( largerValue ) ){
+			sortedSquares[ idx ] = smallerValue * smallerValue;
+			smallerValueIdx++;
+		}else{
+			sortedSquares[idx] = largerValue * largerValue;
+			largerValueIdx--;
+		}
+	}
+  return sortedSquares;
+}
